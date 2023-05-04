ad4b723414c53f33af959037a137f44f5bc9a830
Souvik Pal <palsouvik342@gmail.com>
2023-05-04 16:45:38 +0530

    Second Commit

--- pages/showDataFromCSV.js
+++ pages/showDataFromCSV.js
@@ -1,30 +1,55 @@
 const fs = require('fs');
-const papa = require('papaparse');
-const file = fs.createReadStream('public/data.csv');
+const Papa = require('papaparse');
 
 export async function getServerSideProps() {
-  papa.parse(file,{
-      header: true,
-      worker: true, 
-      step: function(result) {
-          console.log(result)
-          return {
-            props:{
-              result
-            }
-          }
-        },
-        complete: function(results, file) {
-          console.log('parsing completed');
-        }
+  // papa.parse(file,{
+  //     header: true,
+  //     worker: true, 
+  //     step: function(result) {
+  //         console.log(result)
+  //         return {
+  //           props:{
+  //             result
+  //           }
+  //         }
+  //       },
+  //       complete: function(results, file) {
+  //         console.log('parsing completed'); 
+  //         // console.log(results)
+  //     }
+  // });
+  let mainData=[];
+  const file = fs.createReadStream('public/data.csv');
+  Papa.parse(file, {
+    header: true,
+    complete: function(results) {
+      mainData = results.data;
+      console.log(mainData)
+    }
   });
+  // console.log(mainData)
+  return {
+    props: {
+      mainData,
+    }
+  }
 }
 
 
-const showDataFromCSV = () => {
+const showDataFromCSV = ({mainData}) => {
+  console.log(mainData)
   return (
     <div>
-      abc
+      {mainData && mainData.map(data => (
+        <tr>
+          <td>{data.column0}</td>
+          <td>{data.column1}</td>
+          <td>{data.column2}</td>
+          <td>{data.column3}</td>
+          <td>{data.column4}</td>
+          <td>{data.column5}</td>
+        </tr>
+      ))}
     </div>
   )
 }
